0f1173c24b933ad364f261e5f92c3aeed26c8aa5
Gabriel Henrique <ghsouza@outlook.com>
2018-02-26 08:24:04 -0400

    Perguntas 30

--- ShowDoMilhao/ShowDoMilhao/Model/Pergunta.cs
+++ ShowDoMilhao/ShowDoMilhao/Model/Pergunta.cs
@@ -158,10 +158,89 @@ namespace ShowDoMilhao.Model
             p18.AddAlternativa(new Alternativa("BSSID", true));
             lista.Add(p18);
 
+            Pergunta p19 = new Pergunta("O Terminal Server é um serviço que possibilita o acesso remoto a estações e servidores.Levando em consideração o acesso remoto, qual protocolo é utilizado para a comunicação entre o Terminal Server e o cliente do Terminal Server?");
+            p19.AddAlternativa(new Alternativa("RDU"));
+            p19.AddAlternativa(new Alternativa("RDD"));
+            p19.AddAlternativa(new Alternativa("DPR"));
+            p19.AddAlternativa(new Alternativa("RDP", true));
+            lista.Add(p19);
 
+            Pergunta p20 = new Pergunta("É uma das principais peças do computador, onde todos os seus periféricos são acoplados.Possui um conjunto de slots, tais como: ISA, AGP, PCI, PCI Express etc: ");
+            p20.AddAlternativa(new Alternativa("Placa de vídeo"));
+            p20.AddAlternativa(new Alternativa("Placa mãe", true));
+            p20.AddAlternativa(new Alternativa("Placa de rede"));
+            p20.AddAlternativa(new Alternativa("Processador"));
+            lista.Add(p20);
 
+            Pergunta p21 = new Pergunta("Qual o comando utilizado para abrir uma nova guia no navegador Google Chrome ? ");
+            p21.AddAlternativa(new Alternativa("Ctrl+F"));
+            p21.AddAlternativa(new Alternativa("Ctrl+T", true));
+            p21.AddAlternativa(new Alternativa("Ctrl+P"));
+            p21.AddAlternativa(new Alternativa("Ctrl+N"));
+            lista.Add(p21);
 
+            Pergunta p22 = new Pergunta("É um dos principais softwares aplicativos dos sistemas operacionais e utilizado para acessar a Internet, a exemplo de Internet Explorer, Chrome, Safari e Mozilla Firefox: ");
+            p22.AddAlternativa(new Alternativa("Editor de textos"));
+            p22.AddAlternativa(new Alternativa("Planilha eletrônica"));
+            p22.AddAlternativa(new Alternativa("Navegador de internet.", true));
+            p22.AddAlternativa(new Alternativa("Correio eletrônico"));
+            lista.Add(p22);
 
+            Pergunta p23 = new Pergunta("Qual padrão de comunicação de redes sem fio tem por objetivo substituir cabos, permitindo que celulares, palmtops, mouses, entre outros, troquem dados entre si e com o microcomputador sem precisar de cabos ? ");
+            p23.AddAlternativa(new Alternativa("Bluetooth", true));
+            p23.AddAlternativa(new Alternativa("Infravermelho"));
+            p23.AddAlternativa(new Alternativa("Wi-Fi"));
+            p23.AddAlternativa(new Alternativa("Wimax"));
+            lista.Add(p23);
+
+            Pergunta p24 = new Pergunta("O controle de diálogo, o gerenciamento de token e a sincronização são características de qual camada do Modelo OSI/ ISO?");
+            p24.AddAlternativa(new Alternativa("Sessão", true));
+            p24.AddAlternativa(new Alternativa("Transporte"));
+            p24.AddAlternativa(new Alternativa("Enlace de Dados"));
+            p24.AddAlternativa(new Alternativa("Apresentação"));
+            lista.Add(p24);
+
+            Pergunta p25 = new Pergunta("Em uma arquitetura de rede sem fio 802.11, qual processo ocorre após a varredura e decisão de conexão a um dos AP disponíveis?");
+            p25.AddAlternativa(new Alternativa("Autenticação"));
+            p25.AddAlternativa(new Alternativa("Sinalização"));
+            p25.AddAlternativa(new Alternativa("Associação", true));
+            p25.AddAlternativa(new Alternativa("Autorização"));
+            lista.Add(p25);
+
+            Pergunta p26 = new Pergunta("Em relação à segurança da informação, pode-se afirmar que o algoritmo RSA implementa a criptografia e a decriptação de mensagens, utilizando:");
+            p26.AddAlternativa(new Alternativa("chaves simétricas"));
+            p26.AddAlternativa(new Alternativa("chaves simétricas polialfabéticas"));
+            p26.AddAlternativa(new Alternativa("somente chave privada"));
+            p26.AddAlternativa(new Alternativa("chave pública e privada", true));
+            lista.Add(p26);
+
+            Pergunta p27 = new Pergunta("É considerada uma atividade inicial de teste de sistemas que verifica se os componentes funcionam em conjunto, com objetivo de detectar defeitos no sistema.Essa atividade corresponde ao Teste");
+            p27.AddAlternativa(new Alternativa("de integração", true));
+            p27.AddAlternativa(new Alternativa("de releases ou caixa-preta"));
+            p27.AddAlternativa(new Alternativa("estrutural"));
+            p27.AddAlternativa(new Alternativa("de componentes"));
+            lista.Add(p27);
+
+            Pergunta p28 = new Pergunta("A linguagem Java utiliza-se de bytecode e da JVM para conseguir a seguinte característica:");
+            p28.AddAlternativa(new Alternativa("Portabilidade", true));
+            p28.AddAlternativa(new Alternativa("Emulação"));
+            p28.AddAlternativa(new Alternativa("Internacionalização"));
+            p28.AddAlternativa(new Alternativa("Interpretação"));
+            lista.Add(p28);
+
+            Pergunta p29 = new Pergunta("O tipo de ataque a sistemas computacionais que consiste em criar um registro falso de um endereço IP em um servidor de nomes é denominado:");
+            p29.AddAlternativa(new Alternativa("DDOS"));
+            p29.AddAlternativa(new Alternativa("Man-in- the-middle"));
+            p29.AddAlternativa(new Alternativa("Session hijacking"));
+            p29.AddAlternativa(new Alternativa("DNS Spoofing", true));
+            lista.Add(p29);
+
+            Pergunta p30 = new Pergunta("Considerando o endereçamento IPv4, quantos hosts são designados em uma subrede de máscara 255.255.255.224?");
+            p30.AddAlternativa(new Alternativa("30", true));
+            p30.AddAlternativa(new Alternativa("62"));
+            p30.AddAlternativa(new Alternativa("126"));
+            p30.AddAlternativa(new Alternativa("60"));
+            lista.Add(p30);
 
             return lista;
         }
